b141a404af0db8759c536b96f4593035ca0291e4
JMZCH <141981966+JMZCH@users.noreply.github.com>
2025-12-07 14:15:42 -0600

    Delete AP.quejasymasquejas directory

--- AP.quejasymasquejas/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AP.quejasymasquejas.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
